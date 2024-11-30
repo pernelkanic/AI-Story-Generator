6284185fcd5ea8da5834e689efc2d2adfad9d65d
pernelkanic <211001118@rajalakshmi.edu.in>
2024-11-30 13:47:36 +0530

    added image gen base code mfs

---
+++ src/imageGenerator.js
@@ -0,0 +1,43 @@
+const axios = require('axios');
+const FormData = require('form-data');
+const fs = require('fs');
+
+const api_key = process.env.IMAGE_API_KEY;
+const url = "https://api.segmind.com/v1/flux-1.1-pro";
+
+async function GenerateImage(index, text) {
+  const formData = new FormData();
+
+formData.append('seed', 12345);
+  formData.append('width', 1024);
+  formData.append('height', 1024);
+  formData.append('prompt', text);
+  formData.append('aspect_ratio', "1:1");
+  formData.append('output_format', "png");
+  formData.append('output_quality', 80);
+  formData.append('safety_tolerance', 2);
+  formData.append('prompt_upsampling', false);
+
+  try {
+    const response = await axios.post(url, formData, {
+      headers: {
+        'x-api-key': api_key,
+        ...formData.getHeaders(),
+      },
+   
+    });
+    if(response.data){
+        // const buffer = Buffer.from(response.data);
+        // const imagePath = `../generated_images/image_${index}.jpg`
+        // fs.writeFileSync(imagePath, buffer);
+        // console.log(`Image for subtitle ${index} saved as ${imagePath}`);
+        console.log(response.data); 
+         return response.data;
+    }
+  } catch (error) {
+    console.error('Error:', error.response ? error.response.data : error.message);
+  }
+}
+module.exports={
+    GenerateImage
+}
--- src/index.js
+++ src/index.js
@@ -1,18 +1,29 @@
 const { ParseSubtitleFromSrt } = require('./parseSrtFile');
 const { TextToSpeechHandler} = require('./audioGenerator');
+const {GenerateImage}  = require("./imageGenerator");
 
 const inputSRTPath = "../subtitles/minions.srt";
 const rateLimitdelay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 void async function main(){
     const audioFiles = [];
     const subtitleOutput = ParseSubtitleFromSrt(inputSRTPath);
-   
+    const imageFiles = [];
+
     for(const [index , line] of subtitleOutput.entries()){
         const duration = line.endTime - line.startTime;
-        const outputFile = await TextToSpeechHandler(line.text);
-        audioFiles.push(outputFile);
+        const subtitleIndex = index;
+        const tasks = [
+            GenerateImage(subtitleIndex, line.text).then(imageFile => {
+                    imageFiles.push(imageFile);
+                }),
+            TextToSpeechHandler(line.text).then(audioFile => {
+                    audioFiles.push(audioFile);
+                })
+        ];
+        await Promise.all(tasks);
         await rateLimitdelay(5000);
     }
-    //need to handle the merge logic using audioFiles array
+    //todo : need to handle the merge logic using audioFiles array.
+    //todo : merge the audio and subtitle text with the image.
     console.log('Final audio saved as final/final_audio.mp3');
 }();
